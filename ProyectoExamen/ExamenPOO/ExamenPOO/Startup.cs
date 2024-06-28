39de1656c1d12ae679d920fcc7855560d8fc2d59
CristianGmz7 <cristiangomez.cagg78@gmail.com>
2024-06-28 10:53:22 -0600

    CreacionEstructuraProyecto

---
+++ ProyectoExamen/ExamenPOO/ExamenPOO/Startup.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ExamenPOO;
+
+public class Startup
+{
+
+    private IConfiguration Configuration { get; }
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        //Añadir servicios personalizados
+
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
